a42643ba3d065a63f5dbaeb401701ff471839109
IlDavidone <144787644+IlDavidone@users.noreply.github.com>
2025-07-15 17:58:35 +0200

    Testing channel scrollbar functionality with placeholders

--- src/components/ChannelSidebar.jsx
+++ src/components/ChannelSidebar.jsx
@@ -2,19 +2,35 @@ import React from 'react'
 import { useState, useEffect } from "react";
 import { chatroomsState } from "../states/chatroomsState";
 import { channelState } from  "../states/channelState";
+import { Settings2 } from "lucide-react";
 
 const ChannelSidebar = () => {
 
   const { selectedChatroom, fetchChatroomInformations, selectedChatroomInformations } = chatroomsState();
+  const { setCurrentChannel, channels, selectedChannel, areChannelsLoading } = channelState();
 
   useEffect(() => {
     fetchChatroomInformations(selectedChatroom);
   }, [selectedChatroom, fetchChatroomInformations]);
 
+  const mappedChannels = channels.map((channel) => (
+    <></>
+  ))
+
   return (
-    <div className='flex justify-center h-screen w-60 bg-zinc-800'>
-      <div className='flex justify-center items-center w-[100%] h-10 mt-2 mb-2 border-b border-b-zinc-700'>
-        <div className="w-9/10 h-8 text-white">{selectedChatroomInformations.name}</div>
+    <div className='flex flex-col h-screen w-60 bg-zinc-800'>
+      <div className='flex justify-center items-center w-[100%] border-b border-b-zinc-700 hover:bg-zinc-700'>
+        <div className="flex items-center w-9/10 h-12 pl-2 text-white overflow-hidden">{selectedChatroomInformations.name}</div>
+        <div className='flex items-center justify-center pr-2 scale-75'>
+          <Settings2 strokeWidth={1.5} color='white'/>
+        </div>
+      </div>
+      <div className='box-content flex flex-col gap-3 items-center w-[100%] mt-4 mb-2 overflow-y-scroll no-scrollbar overflow-x-hidden'>
+        {Array.from({ length: 100 }).map((_, i) => (
+        <div key={i} className="w-9/10 h-6 pl-2 rounded-md bg-amber-500">
+          Placeholder {i + 1}
+        </div>
+      ))}
       </div>
     </div>
   );
