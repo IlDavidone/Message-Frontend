8366c542456940e618923a8e5f567237d8764299
IlDavidone <144787644+IlDavidone@users.noreply.github.com>
2025-07-15 13:07:17 +0200

    Styling chatrooms sidebar - Placeholder building in progress

--- src/components/ChatroomSidebar.jsx
+++ src/components/ChatroomSidebar.jsx
@@ -2,29 +2,54 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { chatroomsState } from "../states/chatroomsState";
+import { Plus } from "lucide-react";
 
 const ChatroomSidebar = () => {
-  const { areChatroomsLoading, fetchChatrooms, clearChatrooms, chatrooms } = chatroomsState();
-  const [ hasRan, setHasRan ] = useState(false);
+  const { areChatroomsLoading, fetchChatrooms, clearChatrooms, chatrooms } =
+    chatroomsState();
+  const [hasRan, setHasRan] = useState(false);
 
   useEffect(() => {
     if (!hasRan) {
-        fetchChatrooms();
-        setHasRan(true);
+      fetchChatrooms();
+      setHasRan(true);
     } else {
-        return
+      return;
     }
   }, []);
 
   console.log(chatrooms);
 
   const mappedChatrooms = chatrooms.map((chatroom) => (
-    <p key={chatroom._id}>{chatroom.name}</p>
+    <div key={chatroom._id} className="p-1 flex items-center gap-4">
+      <img
+        key={chatroom.image}
+        src={chatroom.image}
+        className="peer h-12 w-12 rounded-full hover:scale-105 transition duration-150 active:scale-95"
+      />
+      <div className="absolute left-18 rounded-sm pt-1 pb-1 pl-2.5 pr-2.5 hidden peer-hover:block bg-zinc-500">
+        <p key={chatroom.name} className="text-white">
+          {chatroom.name}
+        </p>
+      </div>
+    </div>
   ));
 
-  return <div>
-    {mappedChatrooms}
-  </div>;
+  return (
+    <div className="flex flex-col gap-1 items-center pt-1 h-screen w-16 bg-zinc-800">
+      {mappedChatrooms}
+      <div className="flex items-center">
+        <div className="peer flex justify-center items-center bg-slate-800 h-12 w-12 rounded-full hover:bg-slate-700 transition duration-150 active:scale-95">
+          <Plus color="white" />
+        </div>
+        <div className="absolute left-18 rounded-sm pt-1 pb-1 pl-2.5 pr-2.5 hidden peer-hover:block bg-zinc-500">
+          <p key="AddChatroom" className="text-white">
+            Add
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChatroomSidebar;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -13,7 +13,7 @@ const Homepage = () => {
     }
 
   return (
-    <div>
+    <div className="flex">
       <ChatroomSidebar />
       <button onClick={logoutHandler}>Logout</button>
     </div>
